0a562f159f09a7231a08be1dc4ab6a5c0941a165
hotuankiet98 <hotuankiet98@outlook.com.vn>
2021-10-09 18:43:24 +0700

    install

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const morgan=require('morgan')
 const app = express()
- 
+app.use(morgan('combined'))
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send(`<h1>Hello world</h1>`)
 })
  
-app.listen(3000)
+app.listen(8080)
