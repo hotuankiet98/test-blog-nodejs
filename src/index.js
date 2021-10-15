6165634270ccc4fdf396b3fcb3d67b82e8fe7bd1
hotuankiet98 <hotuankiet98@outlook.com.vn>
2021-10-15 19:21:29 +0700

    add file news.hbs

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const path = require('path')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const app = express()
+const port=8080
 //HTTP logger
 app.use(morgan('combined'))
 //Template engine
 app.engine('hbs', exphbs({
     extname:'.hbs'
 }))
+//static file
 app.use(express.static(path.join(__dirname,'public')))
 
 app.set('view engine','hbs')
@@ -19,5 +21,8 @@ app.get('/', function (req, res) {
     res.render('home')
 
 })
+app.get('/news', function (req, res) {
+    res.render('news')
 
-app.listen(8080)
+})
+app.listen(port,()=>console.log(`Example app listening at http://localhost:${port}`))
